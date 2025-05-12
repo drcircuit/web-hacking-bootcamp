9f80db87c02900c3d84f91f77cd7b6de87b6fda6
DrCiRCUiT <workingclasshacker@gmail.com>
2025-05-12 08:47:03 +0200

    scaffold

---
+++ portal/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHackingPortal.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // This can stay empty for now
+        }
+    }
+}
